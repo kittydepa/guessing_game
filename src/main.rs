a6879abc8dacf906c9e22ee8898df219e4c49150
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-02 16:14:05 +0100

    test notes

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ fn main() { // Entry point to the program
     println!("Please input your guess.");
 
     let mut guess = String::new(); // Creating a place to store the user input, as 'guess'. let is also used to create a variable
-
+                                         // String::new(), returns a new instance of String. 'new' creates an empty string
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        .read_line(&mut guess) // read_line takes user input and places it into a string. The & is a reference...(ch4)
+        .expect("Failed to read line"); // part of Result return value, will get a warning without it
     
-    println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess); // One value per set of {}
 }
