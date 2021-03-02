f1a76d193b71bfe0d5094af9bfc7c8e3cb0a5ad6
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-03-02 16:49:12 +0100

    Adding a secret number and conditions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;  // Use the io library from standard. Allows you to accept user input
+use std::cmp::Ordering; // for Less, Greater, Equal
+use rand::Rng; // We bring the Rng trait from the rand crate into this scope
 
 fn main() { // Entry point to the program
     println!("Guess the numbers!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -11,5 +17,14 @@ fn main() { // Entry point to the program
         .read_line(&mut guess) // read_line takes user input and places it into a string. The & is a reference...(ch4)
         .expect("Failed to read line"); // part of Result return value, will get a warning without it
     
+    // 'Shadowing' or reusing the guess var with a new one. Often used when you want to convert value types    
+    let guess: u32 = guess.trim().parse().expect("Please try a number!");
+        
     println!("You guessed: {}", guess); // One value per set of {}
+// match is used to decide what to do based on which variant of Ordering is returned. It is made up of 'arms'
+    match guess.cmp(&secret_number) {    // cmp compares two values, in this case 'guess' and 'secret number'.
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
